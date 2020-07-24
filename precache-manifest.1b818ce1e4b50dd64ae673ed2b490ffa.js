6d39f136a78522a736df3a2db748e2422ae5b46c
MehmetCan75 <mehmetozmenfb@gmail.com>
2020-07-24 21:10:27 +0200

    Updates

---
+++ precache-manifest.1b818ce1e4b50dd64ae673ed2b490ffa.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12c6e0d5d62cc1e91da363f78db37f74",
+    "url": "/Beats/index.html"
+  },
+  {
+    "revision": "1a782d5677246c0f7226",
+    "url": "/Beats/static/css/main.882faaec.chunk.css"
+  },
+  {
+    "revision": "a111e74f2d51019da407",
+    "url": "/Beats/static/js/2.1ae4a90b.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/Beats/static/js/2.1ae4a90b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a782d5677246c0f7226",
+    "url": "/Beats/static/js/main.0e923ce2.chunk.js"
+  },
+  {
+    "revision": "3293c7838b0ade3288e5",
+    "url": "/Beats/static/js/runtime-main.07aabf66.js"
+  },
+  {
+    "revision": "93eb6d3cc60253be41af452033dacdea",
+    "url": "/Beats/static/media/beats.93eb6d3c.jpg"
+  },
+  {
+    "revision": "c719e5ffcb9df7210183555057dd4c2d",
+    "url": "/Beats/static/media/contact.c719e5ff.jpg"
+  },
+  {
+    "revision": "570857a043bb46ade65dc59ee5227a6a",
+    "url": "/Beats/static/media/github.570857a0.png"
+  },
+  {
+    "revision": "a1492555003452e43cd49f83d0ec1c6e",
+    "url": "/Beats/static/media/gmail.a1492555.png"
+  },
+  {
+    "revision": "66e3f37867d70f98205b69899c1a8c0f",
+    "url": "/Beats/static/media/photo-1459233313842-cd392ee2c388.66e3f378.jpeg"
+  },
+  {
+    "revision": "70d551261e756b9c4a9ae369204c6252",
+    "url": "/Beats/static/media/photo-1499415479124-43c32433a620.70d55126.jpeg"
+  },
+  {
+    "revision": "2be486c643e383c9bb1c26e9e6ad3f39",
+    "url": "/Beats/static/media/photo-1525362081669-2b476bb628c3.2be486c6.jpeg"
+  }
+]);
